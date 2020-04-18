185e1651d2103b8d353deaa866434d17d3a02402
Charlie Payne <charliepayne1101@gmail.com>
2020-04-18 14:58:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,7 @@
 def reverse_each_word(sentence)
   new_array = sentence.split(" ")
-  new_sentence = []
-  new_array.each do |string|
-    new_sentence.push(string.reverse)
+  new_array.collect do |string|
+    new_sentence =string.reverse
   end
   new_sentence.join(" ")
 end
